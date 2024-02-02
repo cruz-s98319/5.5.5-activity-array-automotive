6f784638eda8a6632594b88eef1ccf853748391d
Samantha Cruz <cruz.s98319@gmail.com>
2024-02-01 22:23:30 -0800

    Added extended Car class and pseudo logics to mercurySedan.js file

--- mercurySedan.js
+++ mercurySedan.js
@@ -1,5 +1,119 @@
-//this includes the vehicle class as a module
-const VehicleModule = require("./vehicleBaseClass")
+class Vehicle {
+    constructor(make, model, year, color, mileage) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+        this.passenger = 0;
+        this.speed = 0;
+        this.mileage = mileage;
+        this.started = false;
+        this.numberOfWheels = 0;
+    }
+
+    start() {
+        if (this.fuel > 0) {
+            return this.started = true;
+            console.log("engine started...!!!");
+        } else {
+            return this.started = false;
+            console.log("engine cannot start...");
+        }
+    }
+    accelerate() {
+        if (this.started) {
+            if (this.fuel > 0) {
+                console.log(this.speed += 1);
+                this.fuel = this.fuel - 1;
+            } else {
+                console.log("out of fuel.");
+                this.stop();
+            }
+        } else {
+            alert("You need to start the engine first.");
+        }
+    }
+    decelerate() {
+        if (this.started) {
+            if (this.fuel > 0) {
+                if (this.speed > 0) {
+                    console.log(this.speed -= 1);
+                    this.fuel = this.fuel - 1;
+                } else {
+                    console.log(this + " has stopped moving");
+                    this.fuel = this.fuel - 1;
+                }
+            } else {
+                console.log("out of fuel.");
+                this.stop();
+            }
+        } else {
+            alert("You need to start the engine first.");
+        }
+    }
+    stop() {
+        this.started = false;
+    }
+
+    drive() {
+        accelerate();
+    }
+    brake() {
+        decelerate();
+    }
+
+    autoPark()
+    {
+
+    }
+
+    autoDrive()
+    {
+      
+    }
+
+    typeOfVehicle(wheels) {
+        if (this.numberOfWheels == 8 && 8 == wheels) {
+            console.log(this.model + " " + this.make + " is a Truck");
+        } else if (this.numberOfWheels == 4 && 4 == wheels) {
+            console.log(this.model + " " + this.make + " is a CAr");
+        } else if (this.numberOfWheels == 2 && 2 == wheels) {
+            console.log(this.model + " " + this.make + " is a Bike");
+        } else {
+            console.log("Unknown type of vehicle");
+        }
+    }
+}
+
+class Car extends Vehicle {
+    constructor(maximumPassengers, passengers, numberofWheels, maximumSpeed, fuel, scheduleService) {
+        super(make, model, year, color, mileage);
+        this.maximumPassengers = 5;
+        this.passengers = 0;
+        this.numberofWheels = 4;
+        this.maximumSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+    }
+
+    loadPassenger(num) {
+        if (num < this.maximumPassengers) {
+            this.passengers += 1
+        }
+    }
+
+    start() {
+        if (this.started = false && fuel > 0) {
+            this.started = true
+        }
+    }
+
+    scheduleService(mileage) {
+        if (this.mileage >= 3000) {
+            this.scheduleService = true
+        }
+    }
+}
 
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mercury", "Sedan", "1965", "color", "mileage");
